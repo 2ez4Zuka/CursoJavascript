3505d4e82f151f77de6364dc7347f8e259139864
Elder Jefferson, Livisi Carbajal <elderlivisi@gmail.com>
2021-11-27 13:13:09 -0500

    Add files via upload

---
+++ Desafio1/js/script.js
@@ -0,0 +1,6 @@
+let nombre = prompt('Dime tu Nombre')
+let apellido = prompt('Dime tu Apellido')
+
+let sumCaracteres = nombre.length + apellido.length
+
+alert('Hola ' + nombre + ' la suma de caracteres entre tu nombre y apellido es: ' + sumCaracteres)          
